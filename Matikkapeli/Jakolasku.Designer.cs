64399345faeb2c2551087e240e1d53b60208e2a8
Timo Tyrväinen <jiitzi@hotmail.com>
2022-10-15 18:49:37 +0300

    parantelua

--- Matikkapeli/Jakolasku.Designer.cs
+++ Matikkapeli/Jakolasku.Designer.cs
@@ -36,6 +36,7 @@
             this.button3 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,35 +44,37 @@
             // 
             this.label2.BackColor = System.Drawing.Color.White;
             this.label2.Font = new System.Drawing.Font("Showcard Gothic", 14F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(384, 287);
+            this.label2.Location = new System.Drawing.Point(341, 230);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(148, 60);
+            this.label2.Size = new System.Drawing.Size(132, 48);
             this.label2.TabIndex = 14;
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(384, 77);
+            this.textBox1.Location = new System.Drawing.Point(341, 62);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(148, 26);
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
             this.textBox1.TabIndex = 13;
             // 
             // label1
             // 
             this.label1.BackColor = System.Drawing.Color.White;
             this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label1.Location = new System.Drawing.Point(138, 76);
+            this.label1.Location = new System.Drawing.Point(123, 61);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(145, 30);
+            this.label1.Size = new System.Drawing.Size(129, 24);
             this.label1.TabIndex = 12;
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(621, 77);
+            this.button2.Location = new System.Drawing.Point(552, 62);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(145, 48);
+            this.button2.Size = new System.Drawing.Size(129, 38);
             this.button2.TabIndex = 11;
             this.button2.Text = "Laske";
             this.button2.UseVisualStyleBackColor = true;
@@ -79,19 +82,21 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(138, 12);
+            this.button1.Location = new System.Drawing.Point(123, 10);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(145, 45);
+            this.button1.Size = new System.Drawing.Size(129, 36);
             this.button1.TabIndex = 10;
-            this.button1.Text = "Tehtävä";
+            this.button1.Text = "Arvo lasku";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(621, 148);
+            this.button3.Location = new System.Drawing.Point(552, 118);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(145, 64);
+            this.button3.Size = new System.Drawing.Size(129, 51);
             this.button3.TabIndex = 15;
             this.button3.Text = "Valitse uusi peli";
             this.button3.UseVisualStyleBackColor = true;
@@ -101,24 +106,36 @@
             // 
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.Size = new System.Drawing.Size(711, 360);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 16;
             this.pictureBox1.TabStop = false;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(138, 227);
+            this.textBox2.Location = new System.Drawing.Point(123, 182);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(227, 26);
+            this.textBox2.Size = new System.Drawing.Size(202, 22);
             this.textBox2.TabIndex = 17;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(338, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 17);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Anna arvaus";
             // 
             // Jakolasku
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(711, 360);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label2);
@@ -127,6 +144,7 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Jakolasku";
             this.Text = "Jakolasku";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -145,5 +163,6 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
     }
 }
